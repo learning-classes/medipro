02b25086dab09dc1154a94b2e196edf2c4083809
trainde <hassanakintayo1@outlook.com>
2022-06-10 05:12:35 -0400

    DBConnet.java

---
+++ DBConnect.java
@@ -0,0 +1,5 @@
+Dockerfile it is a text file or folder which contains instructions to create a docker images
+
+Image contains all required software, configuration, dependencies, enviroment variable
+
+containers is the run time process of a docker image
